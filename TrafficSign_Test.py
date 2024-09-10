1991ff619e282f44485e699d6a247ad9f022ff2e
NISHAT MAHMUD <nishatrhythm@gmail.com>
2024-09-11 01:13:27 +0600

    Latest update (working fine)

--- TrafficSign_Test.py
+++ TrafficSign_Test.py
@@ -7,151 +7,72 @@ import pickle
 frameWidth = 640  # CAMERA RESOLUTION
 frameHeight = 480
 brightness = 180
-threshold = 0.75  # PROBABLITY THRESHOLD
+threshold = 0.75  # PROBABILITY THRESHOLD
 font = cv2.FONT_HERSHEY_SIMPLEX
+
 ##############################################
 
 # SETUP THE VIDEO CAMERA
-cap = cv2.VideoCapture(0)
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10, brightness)
-# IMPORT THE TRANNIED MODEL
-pickle_in = open("model_trained.p", "rb")  ## rb = READ BYTE
+# url = "http://192.168.0.179:8080/video"  # Replace with the actual URL from the IP Webcam app
+cap = cv2.VideoCapture(1)
+
+# IMPORT THE TRAINED MODEL
+pickle_in = open("model_trained.p", "rb")  # rb = READ BYTE
 model = pickle.load(pickle_in)
 
-
 def grayscale(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return img
-
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def equalize(img):
-    img = cv2.equalizeHist(img)
-    return img
-
+    return cv2.equalizeHist(img)
 
 def preprocessing(img):
     img = grayscale(img)
+    img = equalize(img)
+    img = img / 255  # Normalize to [0, 1]
+    return img
 
-
-img = equalize(img)
-img = img / 255
-return img
-
-
-def getCalssName(classNo):
-    if classNo == 0:
-        return 'Speed Limit 20 km/h'
-    elif classNo == 1:
-        return 'Speed Limit 30 km/h'
-    elif classNo == 2:
-        return 'Speed Limit 50 km/h'
-    elif classNo == 3:
-        return 'Speed Limit 60 km/h'
-    elif classNo == 4:
-        return 'Speed Limit 70 km/h'
-    elif classNo == 5:
-        return 'Speed Limit 80 km/h'
-    elif classNo == 6:
-        return 'End of Speed Limit 80 km/h'
-    elif classNo == 7:
-        return 'Speed Limit 100 km/h'
-    elif classNo == 8:
-        return 'Speed Limit 120 km/h'
-    elif classNo == 9:
-        return 'No passing'
-    elif classNo == 10:
-        return 'No passing for vechiles over 3.5 metric tons'
-    elif classNo == 11:
-        return 'Right-of-way at the next intersection'
-    elif classNo == 12:
-        return 'Priority road'
-    elif classNo == 13:
-        return 'Yield'
-    elif classNo == 14:
-        return 'Stop'
-    elif classNo == 15:
-        return 'No vechiles'
-    elif classNo == 16:
-        return 'Vechiles over 3.5 metric tons prohibited'
-    elif classNo == 17:
-        return 'No entry'
-    elif classNo == 18:
-        return 'General caution'
-    elif classNo == 19:
-        return 'Dangerous curve to the left'
-    elif classNo == 20:
-        return 'Dangerous curve to the right'
-    elif classNo == 21:
-        return 'Double curve'
-    elif classNo == 22:
-        return 'Bumpy road'
-    elif classNo == 23:
-        return 'Slippery road'
-    elif classNo == 24:
-        return 'Road narrows on the right'
-    elif classNo == 25:
-        return 'Road work'
-    elif classNo == 26:
-        return 'Traffic signals'
-    elif classNo == 27:
-        return 'Pedestrians'
-    elif classNo == 28:
-        return 'Children crossing'
-    elif classNo == 29:
-        return 'Bicycles crossing'
-    elif classNo == 30:
-        return 'Beware of ice/snow'
-    elif classNo == 31:
-        return 'Wild animals crossing'
-    elif classNo == 32:
-        return 'End of all speed and passing limits'
-    elif classNo == 33:
-        return 'Turn right ahead'
-    elif classNo == 34:
-        return 'Turn left ahead'
-    elif classNo == 35:
-        return 'Ahead only'
-    elif classNo == 36:
-        return 'Go straight or right'
-    elif classNo == 37:
-        return 'Go straight or left'
-    elif classNo == 38:
-        return 'Keep right'
-    elif classNo == 39:
-        return 'Keep left'
-    elif classNo == 40:
-        return 'Roundabout mandatory'
-    elif classNo == 41:
-        return 'End of no passing'
-    elif classNo == 42:
-        return 'End of no passing by vechiles over 3.5 metric tons'
-
+def getClassName(classNo):
+    classNames = {
+        0: 'Speed Limit 20 km/h', 1: 'Speed Limit 30 km/h', 2: 'Speed Limit 50 km/h', 3: 'Speed Limit 60 km/h',
+        4: 'Speed Limit 70 km/h', 5: 'Speed Limit 80 km/h', 6: 'End of Speed Limit 80 km/h', 7: 'Speed Limit 100 km/h',
+        8: 'Speed Limit 120 km/h', 9: 'No passing', 10: 'No passing for vehicles over 3.5 metric tons',
+        11: 'Right-of-way at the next intersection', 12: 'Priority road', 13: 'Yield', 14: 'Stop', 15: 'No vehicles',
+        16: 'Vehicles over 3.5 metric tons prohibited', 17: 'No entry', 18: 'General caution', 19: 'Dangerous curve to the left',
+        20: 'Dangerous curve to the right', 21: 'Double curve', 22: 'Bumpy road', 23: 'Slippery road',
+        24: 'Road narrows on the right', 25: 'Road work', 26: 'Traffic signals', 27: 'Pedestrians', 28: 'Children crossing',
+        29: 'Bicycles crossing', 30: 'Beware of ice/snow', 31: 'Wild animals crossing', 32: 'End of all speed and passing limits',
+        33: 'Turn right ahead', 34: 'Turn left ahead', 35: 'Ahead only', 36: 'Go straight or right', 37: 'Go straight or left',
+        38: 'Keep right', 39: 'Keep left', 40: 'Roundabout mandatory', 41: 'End of no passing', 42: 'End of no passing by vehicles over 3.5 metric tons'
+    }
+    return classNames.get(classNo, "Unknown")
 
 while True:
+    # READ IMAGE
+    success, imgOriginal = cap.read()
+    if not success:
+        break
 
-# READ IMAGE
-success, imgOrignal = cap.read()
+    # PROCESS IMAGE
+    img = np.asarray(imgOriginal)
+    img = cv2.resize(img, (32, 32))
+    img = preprocessing(img)
+    img = img.reshape(1, 32, 32, 1)
 
-# PROCESS IMAGE
-img = np.asarray(imgOrignal)
-img = cv2.resize(img, (32, 32))
-img = preprocessing(img)
-cv2.imshow("Processed Image", img)
-img = img.reshape(1, 32, 32, 1)
-cv2.putText(imgOrignal, "CLASS: ", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-cv2.putText(imgOrignal, "PROBABILITY: ", (20, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-# PREDICT IMAGE
-predictions = model.predict(img)
-classIndex = model.predict_classes(img)
-probabilityValue = np.amax(predictions)
-if probabilityValue > threshold:
-# print(getCalssName(classIndex))
-cv2.putText(imgOrignal, str(classIndex) + " " + str(getCalssName(classIndex)), (120, 35), font, 0.75, (0, 0, 255), 2,
-            cv2.LINE_AA)
-cv2.putText(imgOrignal, str(round(probabilityValue * 100, 2)) + "%", (180, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
-cv2.imshow("Result", imgOrignal)
+    # PREDICT IMAGE
+    predictions = model.predict(img)
+    classIndex = np.argmax(predictions)
+    probabilityValue = np.amax(predictions)
 
-if cv2.waitKey(1) and 0xFF == ord('q'):
-    break
+    if probabilityValue > threshold:
+        # DISPLAY CLASS NAME AND PROBABILITY
+        cv2.putText(imgOriginal, f"CLASS: {getClassName(classIndex)}", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(imgOriginal, f"PROBABILITY: {round(probabilityValue * 100, 2)}%", (20, 75), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow("Result", imgOriginal)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
